ec9add4bfc7a41eda1f9af6c4040a0ab353d7bd7
Erivanio Vasconcelos <erivanio.vanin@gmail.com>
2016-04-20 10:41:31 -0300

    fix decouple to allowed hosts and debug

--- blog/settings.py
+++ blog/settings.py
@@ -57,10 +57,9 @@ SITE_ID = 1
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
